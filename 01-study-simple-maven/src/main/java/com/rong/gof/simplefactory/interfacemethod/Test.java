2d127306dbc132374d0c3791d9734f8c873aaf18
RQ <1836269477@qq.com>
2023-02-21 20:22:52 +0800

    设计模式：简单工厂模式

---
+++ 01-study-simple-maven/src/main/java/com/rong/gof/simplefactory/interfacemethod/Test.java
@@ -0,0 +1,22 @@
+package com.rong.gof.simplefactory.interfacemethod;
+
+/**
+ * @Author: RQ
+ * @Date: 2023-02-08 11:03:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        Animal dog = AnimalFactory.getAnimal("dog");
+        Animal cat = AnimalFactory.getAnimal("cat");
+        dog.call();
+        dog.methodSame();
+        cat.call();
+        cat.methodSame();
+        /*结果
+        汪汪汪~
+        这是一个动物
+        喵喵喵~
+        这是一个动物
+        */
+    }
+}
